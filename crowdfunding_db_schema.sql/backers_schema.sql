0020c09db55a81b838ef301fde8502533a7e8843
Jingwen Zhou <112013227+jing3508@users.noreply.github.com>
2022-10-19 23:12:01 -0400

    Add files via upload

---
+++ crowdfunding_db_schema.sql/backers_schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "backers" (
+    "backer_id" varchar(10)   NOT NULL,
+    "cf_id" int   NOT NULL,
+    "first_name" varchar(50)   NOT NULL,
+    "last_name" varchar(50)   NOT NULL,
+    "email" varchar(100)   NOT NULL,
+    CONSTRAINT "pk_backers" PRIMARY KEY (
+        "backer_id"
+     )
+);
